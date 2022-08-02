a2a255bb2b2e28b2b3218326c5e27857665a6c20
youness driouch <younes.salmi62@gmail.com>
2022-08-02 22:07:10 +0100

    ttroisieme validatio

--- package.php
+++ package.php
@@ -25,50 +25,20 @@
          <a href="book.php">book</a>
         
       </nav>
+
+
+
   <div id="menu-btn" class="fas fa-bars">
    <!-- nothing -->
   </div>
    </section>
+   <div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+  <h1>packages</h1>
+</div>
    <!-- header section ends  -->
    
-
-<!-- services  section starts -->
- 
-  <section class="services">
-   <h1 class="heading-title">our services</h1>
-   <div class="box-container">
-
-      <div class="box">
-         <img src="images/icon-1.png" alt="">
-         <h3>adventure</h3>
-      </div>
-
-      <div class="box">
-         <img src="images/icon-2.png" alt="">
-         <h3>tour guide</h3>
-      </div>
-
-      <div class="box">
-         <img src="images/icon-3.png" alt="">
-         <h3>trekking</h3>
-      </div>
-      <div class="box">
-         <img src="images/icon-4.png" alt="">
-         <h3>camg fire</h3>
-      </div>
-      <div class="box">
-         <img src="images/icon-5.png" alt="">
-         <h3>off road</h3>
-      </div>
-      <div class="box">
-         <img src="images/icon-6.png" alt="">
-         <h3>camging</h3>
-      </div>
-
-   </div>
-  </section>
-
-<!-- services  section ends -->
+<!-- swiper css file -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
 
 
 <!-- footer section starts -->
@@ -113,10 +83,13 @@
  <div class="credit">created by <span>mr. web developer</span> | all rights  reserved</div>
 </section>
 
+<!-- header section ends -->
+
+
+
 <!-- footer section ends -->
 
-<!-- swiper css file -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+
 <!-- swiper js file -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
 
